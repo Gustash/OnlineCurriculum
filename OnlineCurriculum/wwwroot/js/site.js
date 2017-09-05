9a5978c48f5781c5834fd2bcab58a528e6cc6097
Gustavo Parreira <gustashminecraft@gmail.com>
2017-09-05 12:33:26 +0100

    Added toolbar to open sidenav on smaller screens.

--- OnlineCurriculum/wwwroot/js/site.js
+++ OnlineCurriculum/wwwroot/js/site.js
@@ -1,2 +1,11 @@
 ﻿// Write your Javascript code.
-angular.module('MainApp', ['ngMaterial']);
+angular
+	.module('MainApp', ['ngMaterial'])
+	.controller('AppCtrl', ["$scope", "$timeout", "$mdSidenav", function ($scope, $timeout, $mdSidenav) {
+		$scope.toggleSidenav = buildToggler('sidenav');
+		function buildToggler(componentId) {
+			return function () {
+				$mdSidenav(componentId).toggle();
+			}
+		}
+	}]);
