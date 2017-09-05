09b31d2db99001a5062a51048198c21b12e53e63
Gustavo Parreira <gustashminecraft@gmail.com>
2017-09-05 17:54:55 +0100

    Test iteration through JSON

--- OnlineCurriculum/Pages/Index.cshtml.cs
+++ OnlineCurriculum/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace OnlineCurriculum.Pages
 {
@@ -13,8 +14,18 @@ namespace OnlineCurriculum.Pages
         public void OnGet()
         {
             string data = System.IO.File.ReadAllText(@"wwwroot/data/main-content.json");
-            Content content = JsonConvert.DeserializeObject<Content>(data);
-            Console.WriteLine(content);
+            JObject content = JObject.Parse(data);
+            foreach (var header in content)
+            {
+                string headerTitle = header.Key;
+                foreach (var headerContent in header.Value)
+                {
+                    string title = headerContent["Title"].Value<string>();
+                    string description = headerContent["Description"].Value<string>();
+                    Console.WriteLine("Title: " + title);
+                    Console.WriteLine("Desc: " + description);
+                }
+            }
         }
     }
 }
