eb3928e37eb898805ebd2996382b62253a102c43
Gustavo Parreira <gustashminecraft@gmail.com>
2017-09-08 17:23:27 +0100

    Layout of sidebar and started working on navigation links.

--- OnlineCurriculum/wwwroot/js/site.js
+++ OnlineCurriculum/wwwroot/js/site.js
@@ -3,6 +3,7 @@ angular
 	.module('MainApp', ['ngMaterial'])
 	.controller('AppCtrl', ["$scope", "$timeout", "$mdSidenav", function ($scope, $timeout, $mdSidenav) {
 		$scope.toggleSidenav = buildToggler('sidenav');
+		$scope.toggleRightnav = buildToggler('sidenav-info');
 		function buildToggler(componentId) {
 			return function () {
 				$mdSidenav(componentId).toggle();
